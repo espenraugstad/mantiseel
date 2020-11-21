ce14da286f30b5bf8f093396a51b281c6fa3e4f1
André Pettersen <andrepettersen94@live.no>
2020-11-21 22:01:59 +0100

    det er nå mulig å se preview, klart til å style som ønsket

--- public/js/components/creator.js
+++ public/js/components/creator.js
@@ -1,122 +1,173 @@
 import * as CONST from "../constants.js";
 
-let saveSlide = document.getElementById("save-slide");
-let deleteSlideButton = document.getElementById("delete-slide");
+/**
+ * Application State
+ */
+let activeSlide = null; // Kan inneholde et slide object
+let selectedLayout = CONST.DEFAULT_LAYOUT; // alltid en string
+let selectedTheme = CONST.DEFAULT_THEME; // alltid en string
+let selectedImage = ""; // alltid en string. Tom hvis ikke valgt
+let availableSlides = []; // Array av slide objects som kommer fra back end
 
+// Utility
+const getRandomID = () => Math.floor(Math.random() * 10000);
 //Hent slides (DEBUGGING)
 let getSlidesBTN = document.getElementById("getSlidesBTN");
 getSlidesBTN.addEventListener("click", async () => {
-	const slides = await getSlides()
-	console.log(slides)
+  const slides = await getSlides();
+  console.log(slides);
 });
 
-let activeSlide = null;
-let selectedLayout = CONST.DEFAULT_LAYOUT;
-let selectedTheme = CONST.DEFAULT_THEME;
-let selectedImage = "";
-let availableSlides = [];
-
-/* Lag en slide */
-saveSlide.addEventListener("click", async () => {
-	const slide = createSlideObject();
-	selectedLayout = CONST.DEFAULT_LAYOUT;
-	selectedTheme = CONST.DEFAULT_THEME;
-  await addSlide(slide);
-	await buildAvailableSlides()
-});
-
-
-deleteSlideButton.addEventListener('click', () => deleteSlide(activeSlide.id))
-
-function createSlideObject({giveEmptySlide = false} = {}) {
-	const textInput = document.getElementById("TEXT");
-  const headingInput = document.getElementById("HEADING");
-  const allowsImage = CONST.LAYOUTS[selectedLayout].ALLOWED_CONTENT.includes(
-    "IMAGE"
-  );
-	const slide = {
-    id: availableSlides.length || 0,
-    name: "Slide name",
-    meta: {
-      theme: selectedTheme,
-      layout: selectedLayout,
-    },
-    content: {},
-	};
-	if(giveEmptySlide) return slide;
-
-  if (textInput) slide.content.text = textInput.value;
-  if (headingInput) slide.content.heading = headingInput.value;
-	if (allowsImage && selectedImage) slide.content.image = selectedImage;
-	return slide;
-}
+// Adding listeners to save and delete buttons
+const saveSlide = document.getElementById("save-slide");
+saveSlide.addEventListener("click", createSlide);
+const deleteSlideButton = document.getElementById("delete-slide");
+deleteSlideButton.addEventListener("click", () => deleteSlide(activeSlide.id));
 
 function handleLayoutClick(layoutID) {
+  toggleActiveClass("#layouts", layoutID);
   selectedLayout = layoutID;
-  renderTools();
+  renderTools({ withValues: false });
 }
 
 function handleThemeClick(themeID) {
+  toggleActiveClass("#themes", themeID);
   selectedTheme = themeID;
-  renderTools();
+  renderTools({ withValues: false });
 }
 
+// Creates slide object for back end
+function createSlideObject() {
+  const textInput = document.getElementById("TEXT");
+  const headingInput = document.getElementById("HEADING");
+  const bulletpointsInput = document.getElementById("BULLETPOINTS");
+  const allowsImage = CONST.LAYOUTS[selectedLayout].ALLOWED_CONTENT.includes(
+    "IMAGE"
+  );
+
+  const slide = {
+    id: getRandomID(),
+    name: `Slide name`,
+    meta: {
+      theme: selectedTheme,
+      layout: selectedLayout,
+      createdAt: new Date().getMilliseconds(),
+    },
+    content: {},
+  };
+
+  // Append data if availalle
+  if (bulletpointsInput) {
+    const bulletPoints = getBulletPointsAsArray(bulletpointsInput.value);
+    slide.content.bulletpoints = bulletPoints;
+  }
+  if (textInput) slide.content.text = textInput.value;
+  if (headingInput) slide.content.heading = headingInput.value;
+  if (allowsImage && selectedImage) slide.content.image = selectedImage;
+
+  return slide;
+}
+
+// Converts text from <textarea> element to an array
+function getBulletPointsAsArray(textareaInput) {
+  if (!textareaInput) return null;
+  return textareaInput.split(/\r?\n/).filter((string) => string.length > 0);
+}
+
+function toggleActiveClass(parentElementID, id) {
+  const parent = document.querySelector(parentElementID);
+  Array.from(parent.children).forEach((button) => {
+    if (button.className.includes(id)) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+}
+
+// Layout and Theme
 function renderOptions() {
   const layoutsWrapper = document.querySelector("#layouts");
   const themesWrapper = document.querySelector("#themes");
 
+  // Layout buttons
   Object.entries(CONST.LAYOUTS).forEach(([layoutID, layoutSettings]) => {
     const div = document.createElement("div");
-    div.setAttribute("class", layoutSettings.NAME);
-    div.innerHTML = layoutSettings.NAME;
+    div.setAttribute("class", layoutID);
+    div.setAttribute(
+      "style",
+      `background-image: url(${layoutSettings.PREVIEW}); background-size: contain; background-repeat: no-repeat;`
+    );
     div.addEventListener("click", () => handleLayoutClick(layoutID));
     layoutsWrapper.appendChild(div);
   });
 
+  // Theme buttons
   Object.entries(CONST.THEMES).forEach(([themeID, themeSettings]) => {
     const div = document.createElement("div");
-    div.setAttribute("class", themeSettings.CLASS);
-    div.innerHTML = themeSettings.NAME;
+    div.setAttribute("class", themeID);
+    div.setAttribute("style", `background: ${themeSettings.COLOR}`);
     div.addEventListener("click", () => handleThemeClick(themeID));
     themesWrapper.appendChild(div);
   });
 }
 
-function renderTools() {
+// Text and image upload tools
+function renderTools({ withValues = true } = {}) {
   if (!selectedLayout || !selectedTheme) return;
-	const currentLayout = CONST.LAYOUTS[selectedLayout];
-  const contentToolsWrapper = document.querySelector("#content-tools");
-  // Tømmer det som er i innstillingenes div
-  contentToolsWrapper.innerHTML = "";
+  const currentLayout = CONST.LAYOUTS[selectedLayout];
+  const contentTools = document.querySelector("#content-tools");
+
+  // Always resetting the HTML within #content-tools before filling in updated data
+  contentTools.innerHTML = "";
 
   currentLayout.ALLOWED_CONTENT.forEach((contentType) => {
-		const wrapper = document.createElement("div");
-    const toolElement = getElement(contentType);
-    const labelElement = document.createElement("p");
+    const toolWrapper = document.createElement("div");
+    const toolElement = getElement(contentType, withValues);
+    const labelElement = document.createElement("h4");
     labelElement.innerHTML = CONST.CONTENT_LABEL[contentType];
-    wrapper.appendChild(labelElement);
-    wrapper.appendChild(toolElement);
-    contentToolsWrapper.appendChild(wrapper);
+
+    toolWrapper.setAttribute("class", `${contentType}-wrapper`);
+    toolWrapper.appendChild(labelElement);
+    toolWrapper.appendChild(toolElement);
+    contentTools.appendChild(toolWrapper);
   });
 }
 
-function getElement(contentType) {
+// Creates a HTML element based on the contentType (heading, text, etc)
+// Also inserts values from active slide into text fields
+function getElement(contentType, withValues) {
   switch (contentType) {
     case "HEADING": {
       const element = document.createElement("input");
-			element.setAttribute("id", contentType);
-			if(activeSlide) {
-				element.setAttribute("value", activeSlide.content.heading || '');
-			}
+      element.setAttribute("id", contentType);
+      if (withValues && activeSlide) {
+        element.value = activeSlide.content.heading || "";
+      }
       return element;
     }
 
     case "TEXT": {
       const element = document.createElement("textarea");
-			element.setAttribute("id", contentType);
-			if(activeSlide) {
-				element.innerHTML = activeSlide.content.text || ''
-			}
+      element.setAttribute("id", contentType);
+      if (withValues && activeSlide) {
+        element.value = activeSlide.content.text || "";
+      }
+      return element;
+    }
+
+    case "BULLETPOINTS": {
+      const element = document.createElement("textarea");
+      element.setAttribute("id", contentType);
+      element.setAttribute("rows", "5");
+      if (withValues && activeSlide) {
+        const { bulletpoints } = activeSlide.content;
+        element.value = bulletpoints
+          ? bulletpoints.join("\n")
+          : CONST.DEFAULT.BULLETPOINTS;
+      } else {
+        element.value = CONST.DEFAULT.BULLETPOINTS;
+      }
       return element;
     }
 
@@ -126,9 +177,8 @@ function getElement(contentType) {
       element.setAttribute("type", "file");
       element.setAttribute("accept", "image/*");
       element.addEventListener("change", () => {
-        let [file] = element.files;
-
-        let freader = new FileReader();
+        const file = element.files[0];
+        const freader = new FileReader();
         freader.addEventListener("load", () => {
           selectedImage = freader.result;
         });
@@ -142,36 +192,104 @@ function getElement(contentType) {
   }
 }
 
-async function getSlides() {
-  const url = `/api/getSlides/${localStorage.getItem("pID")}`;
+function buildAvailableSlides() {
+  const availableSlidesWrapper = document.querySelector("#available-slides");
+  availableSlidesWrapper.innerHTML = "";
+  availableSlides.forEach((slide, i) => {
+    const slideButton = document.createElement("div");
+    slideButton.innerHTML = `Slide ${i + 1}`;
+    slideButton.setAttribute("class", slide.id);
+    slideButton.setAttribute("id", "available-slides-button");
 
-  let headers = {
-    "content-type": "application/json",
-    authorization: "Bearer " + sessionStorage.getItem("SID"),
-  };
-
-  const config = {
-    method: "get",
-    headers: headers,
-  };
-
-  let result = await fetch(url, config);
-	let slides = await result.json();
-	
-	const parsedSlides = slides[0].slides.map(slide => {
-		return JSON.parse(slide)
-	}).sort((a, b) => a.id - b.id)
-
-  return parsedSlides;
-
-  /* TODO
-		- Parse informasjon fra slides-variabelen.
-		- Legg til all info om en slide i en div med klasse "slide-div".
-		- Legg til eventlistener på slide-d iv'ene som gjør de klikkbare og gjør
-				det mulig å redigere de igjen.
-		*/
+    slideButton.addEventListener("click", () => {
+      activeSlide = slide;
+      selectedLayout = slide.meta.layout;
+      selectedTheme = slide.meta.theme;
+      toggleActiveClass("#available-slides", slide.id);
+      renderTools();
+      renderSlidePreview();
+    });
+    availableSlidesWrapper.appendChild(slideButton);
+  });
 }
 
+async function updateAvailableSlides() {
+  const slides = await getSlides();
+  availableSlides = slides;
+
+  if (slides.length > 0) {
+    activeSlide = slides[slides.length - 1];
+    deleteSlideButton.style = "display:inline";
+  } else {
+    deleteSlideButton.style = "display:none";
+  }
+  renderSlidePreview();
+  buildAvailableSlides();
+  if (activeSlide) {
+    toggleActiveClass("#available-slides", activeSlide.id);
+  }
+}
+
+function renderSlidePreview() {
+  const slidePreviewElement = document.querySelector("#slide-preview");
+  slidePreviewElement.innerHTML = activeSlide
+    ? getSlidePreview(activeSlide)
+    : "";
+}
+
+function getSlidePreview(slide) {
+  const theme = CONST.THEMES[activeSlide.meta.theme];
+  const layout = CONST.LAYOUTS[activeSlide.meta.layout];
+  const { heading, text, image, bulletpoints } = activeSlide.content;
+
+  // Generate image
+  const imgWrapper = document.createElement("div");
+  const img = new Image();
+  img.setAttribute("class", "slide-image");
+  img.src = image;
+  imgWrapper.appendChild(img);
+
+  switch (slide.meta.layout) {
+    case "LAYOUT_ONE":
+      return `
+        <div class="${layout.CLASS} ${theme.CLASS}">
+          <h1>${heading}</h1>
+        </div>
+      `;
+
+    case "LAYOUT_TWO":
+      return `
+        <div class="${layout.CLASS} ${theme.CLASS}">
+          <h1>${heading}</h1>
+          <p>${text}</p>
+          ${imgWrapper.innerHTML}
+        </div>
+      `;
+
+    case "LAYOUT_THREE":
+      // Generate list
+      const ul = document.createElement("ul");
+      bulletpoints.forEach((bulletpoint) => {
+        const li = document.createElement("li");
+        li.setAttribute("class", "bulletpoint");
+        li.innerHTML = bulletpoint;
+        ul.appendChild(li);
+      });
+
+      return `
+        <div class="${layout.CLASS} ${theme.CLASS}">
+          <h1>${heading}</h1>
+          <ul class="bulletpoints">${ul.innerHTML}</ul>
+          ${imgWrapper.innerHTML}
+        </div>
+      `;
+
+    default:
+      break;
+  }
+}
+
+// Connects to Database
 async function addSlide(slide) {
   const url = "/api/makeSlide";
   let token = sessionStorage.getItem("SID");
@@ -193,16 +311,52 @@ async function addSlide(slide) {
   };
 
   try {
-		await fetch(url, config);
-		await updateAvailableSlides()
+    await fetch(url, config);
+    await updateAvailableSlides();
   } catch (e) {
     console.error(e);
   }
 }
 
+// Connects to Database
+async function getSlides() {
+  const url = `/api/getSlides/${localStorage.getItem("pID")}`;
+
+  let headers = {
+    "content-type": "application/json",
+    authorization: "Bearer " + sessionStorage.getItem("SID"),
+  };
+
+  const config = {
+    method: "get",
+    headers: headers,
+  };
+
+  let result = await fetch(url, config);
+  let slides = await result.json();
+
+  const parsedSlides = slides[0].slides
+    .map((slide) => JSON.parse(slide))
+    .sort((a, b) => a.createdAt - b.createdAt);
+
+  return parsedSlides;
+}
+
+// Connects to Database
+async function createSlide() {
+  const slide = createSlideObject();
+  selectedLayout = CONST.DEFAULT_LAYOUT;
+  selectedTheme = CONST.DEFAULT_THEME;
+  await addSlide(slide);
+  await buildAvailableSlides();
+  toggleActiveClass("#available-slides", slide.id);
+}
+
+// Connects to Database
 async function deleteSlide(slideID) {
-	const slideIndex = availableSlides.findIndex(slide => slide.id === slideID)
-	const url = `/api/deleteSlide?slide_id=${slideID}&presentation_id=${localStorage.getItem("pID")}`;
+  const url = `/api/deleteSlide?slide_id=${slideID}&presentation_id=${localStorage.getItem(
+    "pID"
+  )}`;
   let token = sessionStorage.getItem("SID");
 
   let headers = {
@@ -216,46 +370,18 @@ async function deleteSlide(slideID) {
   };
 
   try {
-		await fetch(url, config);
-		activeSlide = availableSlides[slideIndex - 1] || null
-		selectedLayout = activeSlide?.meta?.layout || CONST.DEFAULT_LAYOUT
-		selectedTheme = activeSlide?.meta?.theme || CONST.DEFAULT_THEME
-		await updateAvailableSlides()
-		renderTools()
+    await fetch(url, config);
+    selectedLayout = activeSlide?.meta?.layout || CONST.DEFAULT_LAYOUT;
+    selectedTheme = activeSlide?.meta?.theme || CONST.DEFAULT_THEME;
+    await updateAvailableSlides();
+    renderTools();
   } catch (e) {
     console.error(e);
   }
 }
 
-function buildAvailableSlides() {
-	const availableSlidesWrapper = document.querySelector('#available-slides')
-	availableSlidesWrapper.innerHTML = '';
-	availableSlides.forEach((slide, i) => {
-		const element = document.createElement('div')
-		element.innerHTML = `Slide ${i + 1}`
-		element.addEventListener('click', () => {
-			activeSlide = slide
-			selectedLayout = slide.meta.layout
-			renderTools();
-		})
-		availableSlidesWrapper.appendChild(element)
-	})
-}
-
-async function updateAvailableSlides() {
-	let slides = await getSlides();
-	availableSlides = slides;
-	if (slides.length > 0) {
-		activeSlide = slides[0]
-		deleteSlideButton.style = 'display:inline'
-	} else {
-		deleteSlideButton.style = 'display:none'
-	}
-	buildAvailableSlides()
-}
-
 window.onload = async function () {
-	await updateAvailableSlides()
+  await updateAvailableSlides();
   renderOptions();
   renderTools();
 };
--- public/js/constants.js
+++ public/js/constants.js
@@ -1,40 +1,50 @@
-export const DEFAULT_THEME = 'THEME_ONE'
-export const DEFAULT_LAYOUT = 'LAYOUT_ONE'
+export const DEFAULT_THEME = "THEME_ONE";
+export const DEFAULT_LAYOUT = "LAYOUT_ONE";
 
 export const THEMES = {
   THEME_ONE: {
     CLASS: "theme-one",
     NAME: "Theme One",
+    COLOR: "#5dc3c2",
   },
   THEME_TWO: {
     CLASS: "theme-two",
     NAME: "Theme Two",
+    COLOR: "#d67172",
   },
 };
 
 export const CONTENT_LABEL = {
-  HEADING: "Heading",
-  TEXT: "Some text",
-  IMAGE: "Photograph",
+  HEADING: "Title",
+  TEXT: "Text",
+  IMAGE: "Image",
+  BULLETPOINTS: "List (separate points on new lines)",
+};
+
+export const DEFAULT = {
+  BULLETPOINTS: `Point 1\nPoint 2\nPoint 3\n`,
 };
 
 export const LAYOUTS = {
   LAYOUT_ONE: {
     DEFAULT_THEME: THEMES.THEME_ONE,
-    CLASS: "layout-button",
+    CLASS: "layout-one",
     NAME: "Layout 1",
     ALLOWED_CONTENT: ["HEADING"],
+    PREVIEW: "../img/presentation1.png",
   },
   LAYOUT_TWO: {
     DEFAULT_THEME: THEMES.THEME_ONE,
-    CLASS: "layout-button",
+    CLASS: "layout-two",
     NAME: "Layout 2",
     ALLOWED_CONTENT: ["HEADING", "TEXT", "IMAGE"],
+    PREVIEW: "../img/presentation2.png",
   },
   LAYOUT_THREE: {
     DEFAULT_THEME: THEMES.THEME_TWO,
-    CLASS: "layout-button",
+    CLASS: "layout-three",
     NAME: "Layout 3",
-    ALLOWED_CONTENT: ["HEADING", "TEXT", "IMAGE"],
+    ALLOWED_CONTENT: ["HEADING", "BULLETPOINTS", "IMAGE"],
+    PREVIEW: "../img/presentation3.png",
   },
 };
